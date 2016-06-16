ce6e0e36c775b59ab94e3181c0899380769e2899
Iñaki Baz Castillo <ibc@aliax.net>
2016-06-16 12:21:44 +0200

    Minor changes

--- update-swis.sh
+++ update-swis.sh
@@ -5,10 +5,10 @@ set -e
 npm cache clean
 
 cd widget/
-rm -rf node_modules./swis/
+rm -rf node_modules/swis/
 npm i swis
 
 cd ..
 cd reflector/
-rm -rf node_modules./swis/
+rm -rf node_modules/swis/
 npm i swis
